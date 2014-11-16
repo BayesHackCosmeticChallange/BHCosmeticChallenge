21779a8f6533490d59ba553f326cfbcd9fde9319
kimi <kimi@kimis-MacBook-Pro-3.local>
2014-11-16 04:42:22 -0800

    change the url

--- cosmetic/cosmetic/settings.py
+++ cosmetic/cosmetic/settings.py
@@ -82,11 +82,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_ROOT = os.path.join(os.getcwd(), "static")
+print STATIC_ROOT
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    '../static/',
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
