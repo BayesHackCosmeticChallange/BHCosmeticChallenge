739009fa6dbaf0d660cb27a8a6a33595631739bc
kimi <kimi@kimis-MacBook-Pro-3.local>
2014-11-15 22:25:59 -0800

    finish the backend

--- cosmetic/chart/views.py
+++ cosmetic/chart/views.py
@@ -7,19 +7,26 @@ from chart.models import Score, Toxicity
 def render_chart(request, **kwargs):
     try:
         #Extract the required parameters for HTTP GET request.
-        product = str(request.GET.get('product'))
-        brand = str(request.GET.get('brand'))
-        brand_name = brand.lower()
-        content = Score.objects.get(brand=brand_name, product=product)
-        toxicity_content = Toxicity.objects.get(cas=content.cas)
-        toxicity = int(toxicity_content.cancer + toxicity_content.female_reproductive +
-                       toxicity_content.male_reproductive + toxicity_content.developmental)
-        json_response = {'brand': content.brand, 'product': content.product,
-                         'efficancy_long': content.efficancy_long,
-                         'efficancy_short': content.efficancy_short,
-                         'smell': content.smell, 'lavant': content.lavant,
-                         'texture': content.texture, 'rincable': content.rincable, 'toxicity': toxicity}
+        all_contents = Score.objects.all()
+        json_response = []
+        for content in all_contents:
+            toxicity_query_set = Toxicity.objects.filter(cas=content.cas)
+            if len(toxicity_query_set) == 0:
+                toxicity = 0
+                pass
+            else:
+                toxicity_content = toxicity_query_set[0]
+                toxicity = int(toxicity_content.cancer + toxicity_content.female_reproductive +
+                               toxicity_content.male_reproductive + toxicity_content.developmental)
+            json_response.append({'brandLabel': content.brand, 'productLabel': content.product,
+                                  'label': content.brand + ' - ' + content.product,
+                                  'score': {'efficancy_long': content.efficancy_long,
+                                            'efficancy_short': content.efficancy_short,
+                                            'smell': content.smell, 'lavant': content.lavant,
+                                            'texture': content.texture, 'rincable': content.rincable,
+                                            'toxicity': toxicity}})
+
         return HttpResponse(json.dumps(json_response), content_type="application/json")
-    except:
-        json_response = {'status': 'failure', 'status_msg': "Something wrong"}
+    except Exception as e:
+        json_response = {'status': 'failure', 'status_msg': str(e)}
         return HttpResponse(json.dumps(json_response), content_type="application/json")
---
+++ cosmetic/static/js/radarPlot.js
@@ -0,0 +1,97 @@
+// svg part
+var n_branch = 6;
+var height= 450;
+var width = 450;
+var viewBox = "0 40 450 450";
+var rcenter = 0;
+var rhandle = 3;
+var rtext = 60;
+var base_color = "#fff";
+var color = base_color;
+
+var feature1 = 'feature1'
+var feature2 = 'feature2'
+var feature3 = 'feature3'
+var feature4 = 'feature4'
+var feature5 = 'feature5'
+var feature6 = 'feature6'
+
+var branch_len = height / 2 - 2*rtext;
+
+var handles = [];
+
+var names = [feature1, feature2,feature3, feature4, feature5, feature6];
+
+var svgContainer = d3.select("#main").append("svg")
+    .attr({
+        "width": '100%',
+        "height": '100%'
+    })
+    .attr("viewBox", viewBox)
+    .attr("preserveAspectRatio", "xMidYMin")
+
+var polygon = svgContainer.append("polygon")
+    .attr("stroke", color)
+    .attr("fill", color)
+    .attr("fill-opacity", "0.2")
+    .attr("stroke-width","1");
+
+for (i=0 ; i<n_branch ; i++) {
+
+    svgContainer.append("line")        
+        .attr("stroke",color)
+        .attr("stroke-width","1")
+        .attr("stroke-linecap", "round")
+        .attr("x1",width/2)
+        .attr("y1",height/2)
+        .attr("x2",width/2 + (branch_len + rcenter) * Math.cos(2 * Math.PI / n_branch * i - Math.PI / 2))
+        .attr("y2",height/2 + (branch_len + rcenter) * Math.sin(2 * Math.PI / n_branch * i - Math.PI / 2));
+
+    svgContainer.append("text")
+        .text(names[i])
+        .attr('id', names[i])
+        .attr("fill",color)
+        .attr("font-size", "1rem")
+        .attr('text-anchor', 'middle')
+        .attr('font-weight', 'light')
+        .attr("font-family","Lato")
+        .attr("x",width/2 + (branch_len + rtext + rcenter) * Math.cos(2 * Math.PI / n_branch * i - Math.PI / 2))
+        .attr("y",height/2 + (branch_len + rtext + rcenter) * Math.sin(2 * Math.PI / n_branch * i - Math.PI / 2));
+
+    svgContainer.append("text")
+        .text('5')
+        .attr('id', 'value' + i)
+        .attr("fill",color)
+        .attr("font-size", "1.5rem")
+        .attr('text-anchor', 'middle')
+        .attr('font-weight', 'light')
+        .attr("font-family","Lato")
+        .attr("x",width/2 + (branch_len + rtext + rcenter) * Math.cos(2 * Math.PI / n_branch * i - Math.PI / 2))
+        .attr("y",height/2 + (branch_len + rtext + rcenter) * Math.sin(2 * Math.PI / n_branch * i - Math.PI / 2) + 40);
+
+    var handle = svgContainer.append("circle")
+        .attr("fill",color)
+        .attr("cx",width/2 + (rcenter + branch_len/2) * Math.cos(2 * Math.PI / n_branch * i - Math.PI / 2))
+        .attr("cy",height/2 + (rcenter + branch_len/2) * Math.sin(2 * Math.PI / n_branch * i - Math.PI / 2))
+        .attr("r", rhandle)
+        .attr("id", "handle" + i)
+    handles.push(handle);
+}
+
+function update_polygon() {
+    var handle_pol = "";
+    for (i=0 ; i<n_branch ; i++) {
+        handle_pol += handles[i].attr("cx")+","+handles[i].attr("cy")+" ";
+    }
+    polygon.attr("points", handle_pol);
+}
+
+function update_value(selected, r) {
+    d3.select("#value" + selected)
+        .text(r);
+}
+
+update_polygon();
+
+
+
