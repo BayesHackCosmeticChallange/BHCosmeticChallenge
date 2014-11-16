8c0ad8183ef7e80f8d9ae900816e46adb75de023
kimi <kimi@kimis-MacBook-Pro-3.local>
2014-11-16 05:07:36 -0800

    add the mysql config

--- cosmetic/cosmetic/settings.py
+++ cosmetic/cosmetic/settings.py
@@ -82,14 +82,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = os.path.join(os.getcwd(), "static")
-print STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '../static/',
+    STATIC_ROOT,
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
